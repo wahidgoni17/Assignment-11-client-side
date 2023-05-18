d49ea101740b0b2b8221ba2d5907a5b153a643f9
wahidgoni17 <imwahidgoni17@gmail.com>
2023-05-18 22:50:02 +0600

    toys send to database

--- src/Components/ToysRoute/Addtoys.jsx
+++ src/Components/ToysRoute/Addtoys.jsx
@@ -2,15 +2,49 @@ import React, { useContext } from "react";
 import { AuthContext } from "../Routers/AuthProvider";
 
 const Addtoys = () => {
-    const {user} = useContext(AuthContext)
+  const { user } = useContext(AuthContext);
+  const handleAddToys = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = user?.email;
+    const sellerName = user?.displayName;
+    const toyName = form.toyName.value;
+    const toyPhoto = form.toyPhoto.value;
+    const subCatagory = form.subCatagory.value;
+    const price = form.price.value;
+    const quantity = form.quantity.value;
+    const ratings = form.ratings.value;
+    const details = form.details.value;
+    const toyData = {
+      email,
+      sellerName,
+      toyName,
+      toyPhoto,
+      subCatagory,
+      price,
+      quantity,
+      ratings,
+      details,
+    };
+    console.log(toyData);
+    fetch("http://localhost:4000/toys", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(toyData)
+    });
+  };
   return (
     <>
       <div className="text-center my-10">
         <h1 className="text-4xl font-bold">Add Toys Here</h1>
       </div>
       <div className="p-10 bg-slate-100">
-        <h1 className="text-center my-5 text-3xl font-semibold">Give Some Information</h1>
-        <form>
+        <h1 className="text-center my-5 text-3xl font-semibold">
+          Give Some Information
+        </h1>
+        <form onSubmit={handleAddToys}>
           <div className="flex gap-5 my-5">
             <div className="form-control w-1/2">
               <input
@@ -96,7 +130,11 @@ const Addtoys = () => {
             </div>
           </div>
           <div className="form-control mt-6">
-            <input className="btn btn-outline btn-accent" type="submit" value="Add Toy" />
+            <input
+              className="btn btn-outline btn-accent"
+              type="submit"
+              value="Add Toy"
+            />
           </div>
         </form>
       </div>
