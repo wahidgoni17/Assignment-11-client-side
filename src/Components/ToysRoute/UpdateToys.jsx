ef9537e2faa8576f3365a95932f1d23e2e76ced8
wahidgoni17 <imwahidgoni17@gmail.com>
2023-05-19 20:22:26 +0600

    update toy added

--- src/Components/ToysRoute/UpdateToys.jsx
+++ src/Components/ToysRoute/UpdateToys.jsx
@@ -3,34 +3,54 @@ import { useLoaderData } from "react-router-dom";
 
 const UpdateToys = () => {
   const toydata = useLoaderData();
-  const {
-    subCatagory,
-    price,
-    quantity,
-    ratings,
-    details,
-  } = toydata
+  const { _id, subCatagory, price, quantity, details, toyName } = toydata;
+  const handleUpdate = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const toyName = form.toyName.value;
+    const subCatagory = form.subCatagory.value;
+    const price = form.price.value;
+    const quantity = form.quantity.value;
+    const details = form.details.value;
+    const updatedInfo = { toyName, subCatagory, price, quantity, details };
+    fetch(`http://localhost:4000/toys/${_id}`, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(updatedInfo),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.modifiedCount > 0) {
+          confirm("Updated Successfully");
+        }
+      });
+  };
   return (
     <>
       <div className="text-center my-5">
         <h1 className="text-4xl font-bold">Update Toys</h1>
       </div>
-      <div className="p-10">
-        <form>
+      <div className="p-10 bg-slate-100">
+        <form onSubmit={handleUpdate}>
           <div className="flex gap-5 my-5">
             <div className="form-control w-1/2">
               <input
                 type="text"
-                name="quantity"
-                placeholder="Available Quantity"
+                name="toyName"
+                placeholder="Toy Name"
+                defaultValue={toyName}
                 className="input text-white input-bordered"
               />
             </div>
             <div className="form-control w-1/2">
               <input
                 type="text"
-                name="ratings"
-                placeholder="Ratings"
+                name="subCatagory"
+                placeholder="Sub Catagory"
+                defaultValue={subCatagory}
                 className="input text-white input-bordered"
               />
             </div>
@@ -40,6 +60,7 @@ const UpdateToys = () => {
               <input
                 type="text"
                 name="quantity"
+                defaultValue={quantity}
                 placeholder="Available Quantity"
                 className="input text-white input-bordered"
               />
@@ -47,11 +68,30 @@ const UpdateToys = () => {
             <div className="form-control w-1/2">
               <input
                 type="text"
-                name="ratings"
-                placeholder="Ratings"
+                name="price"
+                placeholder="Price"
+                defaultValue={price}
                 className="input text-white input-bordered"
               />
             </div>
+          </div>
+          <div>
+            <div className="form-control">
+              <input
+                type="text"
+                name="details"
+                placeholder="Description"
+                defaultValue={details}
+                className="input text-white input-bordered h-28"
+              />
+            </div>
+          </div>
+          <div className="form-control mt-6">
+            <input
+              className="btn btn-outline btn-accent"
+              type="submit"
+              value="Update Toy"
+            />
           </div>
         </form>
       </div>
